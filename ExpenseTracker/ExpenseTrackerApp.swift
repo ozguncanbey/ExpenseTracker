45b36c2715e6ecc8defddd6b03094f59b747cca6
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2023-09-05 16:24:13 +0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Özgün Can Beydili on 5.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
